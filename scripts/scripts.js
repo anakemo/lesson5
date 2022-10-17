e441154799a46fb15453d3e5cc6d5d525d5c5e6d
anakemo <anakemoklidze1993@gmail.com>
2022-10-17 10:11:33 +0200

    first commit

---
+++ scripts/scripts.js
@@ -0,0 +1,127 @@
+// #1
+
+// let array = [ 5, 25, 89,120,36];
+// array.push("javascript", "phyton");
+
+// console.log(array);
+// array.unshift("html", "css");
+// console.log(array);
+
+// console.log(array.length);
+// let array = [ 5, 25, 89,120,36];
+// array.splice(0,1);
+// array.splice(-1,1);
+// console.log(array);
+
+// #2
+
+// let xili=["fortoxali", "banani", "msxali"];
+// console.log(xili.length);
+// xili.push("vashli", "ananasi");
+// console.log(xili);
+// xili.unshift("sazamtro");
+// console.log(xili);
+// console.log(xili.length);
+// xili.splice(3,0,"mango");
+// console.log(xili);
+// xili.shift(1);
+// xili.pop(-1);
+// console.log(xili);
+// console.log(xili.length);
+
+// #3
+// let array=[12,25,3,6,8,14,7,23];
+// let newArray=array.map(function(item){
+
+//     return(item/3);
+// });
+// console.log(newArray);
+// #4
+// let array=["hello", 125, "javascript", "html", 43, "css", "scss", "bootstrap",88, 59, "python"];
+// let newArray=array.filter(function(item){
+//     return item>0;
+
+
+// });
+// console.log(newArray);
+// #5
+// let languages = ["html", "css", "javascript", "python", "php"];
+// let newLanguages=languages.filter(function(item){
+//     return item.length> 3;
+// });
+// console.log(newLanguages);
+
+// #6
+
+// ???
+
+
+
+// #7
+// let words =["madrid", "rome", "milan", "berlin"];
+
+// let newWord=words.filter(word=>word.includes("m"));
+// console.log(newWord);
+    
+
+// #8
+// let array1=[1,2];
+// let array2=[3,4];
+// let array3=[5,6];
+// let arrayAll=array1.concat(array2).concat(array3);
+// console.log(arrayAll);
+
+// #9
+
+// let number=[1,2,3,4,5];
+
+// let sum =number.reduce((accumulator,value)=>{
+//     return accumulator+ value;
+// });
+// console.log(sum);
+// #10
+// let numbers=[-2,5,10,-5,-1,1,3,4,-15];
+// let newNumbers=numbers.filter(function(item){
+//     return item>0;
+// });
+// console.log(newNumbers);
+
+// #11
+// let array=[-1,-2,-3,4];
+// function checkNum(element){
+//     return element.value>0;
+// };
+// array.some(checkNum);
+// let array=[-1,-2,-3,4];
+// array.some((x)=>x>0);
+
+
+// ver avamushave :(((
+
+// #12
+// let array = [23,45,32,5,67,7,3,98];
+// let newArray=array.sort(function(x,y){
+//     return y-x;
+   
+
+// });
+// console.log(array[array.length-1]);
+
+// #13
+
+
+// let array = [1,2,3,4,5];
+// array.splice(3,0,   "a", "b", "c");
+// console.log(array);
+
+   
+
+
+
+
+
+
+
+
+
+
